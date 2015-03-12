445cbcf45e261198432555f5904c7901147432b4
craigminihan <craig@ripcordsoftware.com>
2015-03-12 22:19:37 +0000

    Test the gzipped response files with gunzip

--- src/httpserver/test.sh
+++ src/httpserver/test.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
-dist/Debug/GNU-Linux-x86/httpserver &
 
-mkdir -p test.tmp
-cd test.tmp
+SERVER_ROOT=${PWD}/dist/Debug/GNU-Linux-x86
+WEB_ROOT=${SERVER_ROOT}/www
+
+${SERVER_ROOT}/httpserver &
+
+trap 'kill %1' 0
+
+mkdir -p tmp.test
+cd tmp.test
 
 # spider the web files with wget
-wget -4 -r http://localhost:10024/ -R js,css,jpg,png
+mkdir spider.test
+cd spider.test
+wget -4 -r --no-host-directories http://localhost:10024/ 
+cd ..
 
 # request the web files with curl
-uris=(`find ../dist/Debug/GNU-Linux-x86/www/ -type f -printf "%P\n"`)
+mkdir gzip.test
+cd gzip.test
+uris=(`find $WEB_ROOT -type f -printf "%P\n"`)
 for uri in "${uris[@]}"
 do
-    curl --header "Accept-Encoding: gzip" http://localhost:10024/${uri} -O
-done
+    curl -4 --header "Accept-Encoding: gzip" http://localhost:10024/${uri} -o ${uri}.gz --create-dirs
+    if [ "$?" -ne 0 ]; then exit $?; fi
 
-kill %1
+    # if the file is gzipped then test it
+    file ${uri}.gz | grep gzip
+    if [ "$?" -eq 0 ]; then
+        gunzip -t $uri.gz || exit $?
+    fi
+done
+cd ..
 
 cd ..
-rm -rf test.tmp
+rm -rf tmp.test
