72b424a36acc3daa272c755edcc664e15211cc80
craigminihan <craig@ripcordsoftware.com>
2015-03-12 18:25:47 +0000

    Correct the travis detect boolean if test

--- coverage.sh
+++ coverage.sh
@@ -2,7 +2,7 @@
 lcov -b src/libhttpserver --directory src/libhttpserver/build/Debug/GNU-Linux-x86/ --capture --output-file coverage.info
 lcov --remove coverage.info 'tests/*' '/usr/*' '*/externals/*' --output-file coverage.info
 lcov --list coverage.info
-if [ "$CI" == "true" && "$TRAVIS" == "true" ]; then
+if ( "$CI" == "true" && "$TRAVIS" == "true" ); then
     coveralls-lcov coverage.info
 else
     rm -rf coverage
