943a9ba4fe23a39a52bde67bf0608474cf73ddcd
craigminihan <craig@ripcordsoftware.com>
2015-03-12 17:29:21 +0000

    Added a curl test for gzip response testing

--- src/httpserver/test.sh
+++ src/httpserver/test.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 dist/Debug/GNU-Linux-x86/httpserver &
+
 mkdir -p test.tmp
 cd test.tmp
+
+# spider the web files with wget
 wget -4 -r http://localhost:10024/ -R js,css,jpg,png
+
+# request the web files with curl
+uris=(`find ../dist/Debug/GNU-Linux-x86/www/ -type f -printf "%P\n"`)
+for uri in "${uris[@]}"
+do
+    curl --header "Accept-Encoding: gzip" http://localhost:10024/${uri} -O
+done
+
 kill %1
+
 cd ..
 rm -rf test.tmp
