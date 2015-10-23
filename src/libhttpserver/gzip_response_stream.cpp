27055c3394c4e5ee6078cae0a6e62b2be0f43fe5
craigminihan <craig@ripcordsoftware.com>
2015-10-23 10:38:11 +0100

    Fix response compression where outbound buffer isn't big enough

--- src/libhttpserver/gzip_response_stream.cpp
+++ src/libhttpserver/gzip_response_stream.cpp
@@ -1,5 +1,7 @@
 #include "gzip_response_stream.h"
 
+#include <array>
+
 rs::httpserver::GzipResponseStream::GzipResponseStream(Stream& stream) : stream_(stream) {
     ::memset(&zStream_, 0, sizeof(zStream_));
      ::deflateInit2(&zStream_, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 31, 9, Z_DEFAULT_STRATEGY);
@@ -11,40 +13,50 @@ rs::httpserver::GzipResponseStream::~GzipResponseStream() {
 
 int rs::httpserver::GzipResponseStream::Write(const byte* buffer, int offset, int count) {    
     if (count > 0) {
-        Stream::byte zBuffer[2048];
-
-        auto prevTotalOut = zStream_.total_out;
+        std::array<Stream::byte, 2048> zBuffer;
 
         zStream_.next_in = const_cast<byte*>(buffer + offset);
         zStream_.avail_in = count;
-        zStream_.next_out = zBuffer;
-        zStream_.avail_out = sizeof(zBuffer);
 
-        if (::deflate(&zStream_, Z_SYNC_FLUSH) != Z_OK) {
-            throw GzipCompressionException();
+        while (zStream_.avail_in > 0) {
+            zStream_.next_out = zBuffer.data();
+            zStream_.avail_out = zBuffer.size();
+
+            if (::deflate(&zStream_, Z_NO_FLUSH) != Z_OK) {
+                throw GzipCompressionException();
+            }
+
+            auto size = zBuffer.size() - zStream_.avail_out;
+            if (size > 0) {
+                stream_.Write(zBuffer.data(), 0, size);
+            }
         }
-        
-        stream_.Write(zBuffer, 0, zStream_.total_out - prevTotalOut);
     }    
     
     return count;
 }
 
 void rs::httpserver::GzipResponseStream::Flush() {     
-    Stream::byte zBuffer[2048];
-    
-    auto prevTotalOut = zStream_.total_out;
+    std::array<Stream::byte, 2048> zBuffer;
     
     zStream_.next_in = nullptr;
     zStream_.avail_in = 0;
-    zStream_.next_out = zBuffer;
-    zStream_.avail_out = sizeof(zBuffer);
     
-    if (::deflate(&zStream_, Z_FINISH) != Z_STREAM_END) {
-        throw GzipCompressionException();
-    }
+    auto status = Z_OK;
+    while (status == Z_OK) {
+        zStream_.next_out = zBuffer.data();
+        zStream_.avail_out = zBuffer.size();
+
+        status = ::deflate(&zStream_, Z_FINISH);
+        if (status != Z_OK && status != Z_STREAM_END) {
+            throw GzipCompressionException();
+        }
         
-    stream_.Write(zBuffer, 0, zStream_.total_out - prevTotalOut);
+        auto size = zBuffer.size() - zStream_.avail_out;
+        if (size > 0) {
+            stream_.Write(zBuffer.data(), 0, size);
+        }
+    }
     
     stream_.Flush(); 
 }
