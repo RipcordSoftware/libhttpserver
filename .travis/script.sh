8218df61d2134e3ccc70f3a006c8cca9a84a0c2e
craigminihan <craig@ripcordsoftware.com>
2016-09-29 21:56:59 +0100

    Correct env var refs in .travis/script.sh

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,4 +1,4 @@
-make CC=${_CC} CXX=$(_CXX) -j 2 all && make CC=${_CC} CXX=$(_CXX) -j 2 test
+make CC=${_CC} CXX=${_CXX} -j 2 all && make CC=${_CC} CXX=${_CXX} -j 2 test
 
 if [[ "${COV}" != "" ]]; then
     ./coverage.sh ${_COV}
