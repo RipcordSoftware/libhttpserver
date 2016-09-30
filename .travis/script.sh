327347ab1a82d2c1dd05f21009e43bebb8f4adca
craigminihan <craig@ripcordsoftware.com>
2016-09-30 16:20:00 -0700

    Tell the linker where the boost libs are * use on osx

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,4 +1,4 @@
-make CC=${_CC} CXX=${_CXX} -j 2 all && make CC=${_CC} CXX=${_CXX} test || exit $?
+make CC=${_CC} CXX=${_CXX} LDFLAGS=${_LDFLAGS} -j 2 all && make CC=${_CC} CXX=${_CXX} LDFLAGS=${_LDFLAGS} test || exit $?
 
 if [[ "${COV}" != "" ]]; then
     ./coverage.sh ${_COV}
