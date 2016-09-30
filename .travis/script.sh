d4e1edb8a1ad1d9d9dd3a45d8359541ed16fc33a
craigminihan <craig@ripcordsoftware.com>
2016-09-30 16:52:43 +0100

    Correctly detect build failure * don't treat OSX as a fail for the moment

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,4 +1,4 @@
-make CC=${_CC} CXX=${_CXX} -j 2 all && make CC=${_CC} CXX=${_CXX} test
+make CC=${_CC} CXX=${_CXX} -j 2 all && make CC=${_CC} CXX=${_CXX} test || exit $?
 
 if [[ "${COV}" != "" ]]; then
     ./coverage.sh ${_COV}
